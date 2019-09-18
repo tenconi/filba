a67e81dcce33d9a7278dd47e9b7a464b67bd7179
tenconi <47565305+tenconi@users.noreply.github.com>
2019-09-18 13:49:13 -0300

    ajustes para 425px - servicios

--- contacto.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$destino="tenco@tenco.com.ar";
-	$nombre=$_POST["nombre"];
-	$correo=$_POST["email"];
-	$mensaje=$_POST["mensaje"];
-	$contenido="Nombre: " . $nombre . "\nCorreo: " . $correo "\nMensaje: " . $mensaje ;
-	mail($destino, "Contacto Web", $contenido);
-	header("Location: index.html");
-?>
